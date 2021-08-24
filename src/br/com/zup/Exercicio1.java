f01068125ae9b624a8cc6579d5a3b8e29a500c3c
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-24 15:28:12 -0300

    Finalizando Exercicio 1

--- src/br/com/zup/Exercicio1.java
+++ src/br/com/zup/Exercicio1.java
@@ -2,6 +2,11 @@ package br.com.zup;
 
 public class Exercicio1 {
     public static void main(String[] args) {
-        //gi
+        //Fazer um programa que escreva Olá, Mundo!
+
+        String nome = "Dorayen";
+
+        System.out.print("Alô, Mundo! e Alô, " +nome +"!");
+
     }
 }
