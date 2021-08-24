ee816bbf7c59ce963754821e20172d9a129c8511
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-24 16:40:56 -0300

    Mostrando Resultados Ex 005

--- src/br/com/zup/Exercicio5.java
+++ src/br/com/zup/Exercicio5.java
@@ -3,11 +3,13 @@ package br.com.zup;
 public class Exercicio5 {
     public static void main(String[] args) {
         //Adicionando as variáveis
-
+        float temperaturaFahrenheit = 78;
 
         //Calculo da temperatura
-
+        float celsius = (temperaturaFahrenheit-32) / 1.800F;
 
         //Resultados Usuario
+        System.out.println("Convirtiendo " + temperaturaFahrenheit +"° em °Celsius");
+        System.out.println("O resultado é: " +celsius);
     }
 }
