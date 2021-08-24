cb8cd5677071ba9d5a7e6c331405df0ae66ee929
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-24 16:05:45 -0300

    Adicionando Variáveis EX 004

---
+++ src/br/com/zup/Ejercicio4.java
@@ -0,0 +1,11 @@
+package br.com.zup;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        //Adicionando as variáveis
+        int horasTrabalhadas = 40;
+        float valorHora = 10.5F;
+
+
+    }
+}
