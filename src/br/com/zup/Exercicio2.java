af5e33e5e951ad9472ab4acb52fc0894ce6396b9
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-24 15:37:25 -0300

    Adicionando variáveis 2 exercicio

---
+++ src/br/com/zup/Exercicio2.java
@@ -0,0 +1,14 @@
+package br.com.zup;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+    //Adicionando as variáveis
+    float nota1 = 7.5F;
+    float nota2 = 8;
+    float nota3 = 9.3F;
+    float nota4 = 8.2F;
+
+    float somaNotas =
+    float media = somaNotas;
+    }
+}
