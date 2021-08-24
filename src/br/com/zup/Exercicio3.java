f2c026f1284f9bc7258663555d56c40a4d7c0b3c
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-24 15:52:05 -0300

    Adicionando as variáveis

--- src/br/com/zup/Exercicio3.java
+++ src/br/com/zup/Exercicio3.java
@@ -2,6 +2,11 @@ package br.com.zup;
 
 public class Exercicio3 {
     public static void main(String[] args) {
+        //Adicionando variáveis
+        float valorEmMetros = 18.6F;
+        int centimetro = 100;
+        float convertir = valorEmMetros * centimetro;
+
 
     }
 }
