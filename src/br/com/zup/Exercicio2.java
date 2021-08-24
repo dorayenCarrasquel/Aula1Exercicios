c47deda3d429fa0231f664b7e785d13ff715ed0a
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-24 15:41:54 -0300

    Mostrando as notas

--- src/br/com/zup/Exercicio2.java
+++ src/br/com/zup/Exercicio2.java
@@ -8,7 +8,10 @@ public class Exercicio2 {
     float nota3 = 9.3F;
     float nota4 = 8.2F;
 
-    float somaNotas =
-    float media = somaNotas;
+    float somaNotas = nota1 + nota2 + nota3 + nota4;
+    float media = somaNotas / 4;
+
+    // Resultados da Media
+        System.out.println("Suas notas este semestre foram " +nota1 +", " +nota2 +", " +nota3 +" e " + nota4);
     }
 }
