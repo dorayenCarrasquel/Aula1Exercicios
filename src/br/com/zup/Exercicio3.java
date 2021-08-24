daf939e5edcc3144abb011a36765a8738327de66
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-08-24 15:58:06 -0300

    Finalizando resultado conversão mts * cm

--- src/br/com/zup/Exercicio3.java
+++ src/br/com/zup/Exercicio3.java
@@ -3,10 +3,14 @@ package br.com.zup;
 public class Exercicio3 {
     public static void main(String[] args) {
         //Adicionando variáveis
-        float valorEmMetros = 18.6F;
+        float valorEmMetros = 4.8F;
         int centimetro = 100;
-        float convertir = valorEmMetros * centimetro;
+        float convertir = centimetro * valorEmMetros;
 
+        //Resultado da conversão
+        System.out.println("Metros a convertir: " + valorEmMetros);
+        System.out.println("************");
+        System.out.println(valorEmMetros +"mts em centimetros são: " +convertir +"cm");
 
     }
 }
